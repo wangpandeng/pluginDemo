e858dc3d34cfb0c167e13f508680ff8f40e9107e
wangpandeng <980431150@qq.com>
2020-08-24 00:54:09 +0800

    修改逻辑，添加备注

--- app/src/main/java/com/example/pluginapp/hook/AppInstrumentation.java
+++ app/src/main/java/com/example/pluginapp/hook/AppInstrumentation.java
@@ -24,7 +24,7 @@ public class AppInstrumentation extends Instrumentation {
         this.pluginContext = pluginContext;
     }
 
-    //Instrumentation代理实例替换MainThread和Activity类中的mInstrumentation
+    //第一步，Instrumentation代理实例替换MainThread和Activity类中的mInstrumentation
     public static void inject(Activity activity, PluginContext pluginContext) {
         //Reflect是反射工具
         Reflect reflect = Reflect.on(activity);
@@ -35,15 +35,42 @@ public class AppInstrumentation extends Instrumentation {
         Reflect.on(activity).set("mInstrumentation", appInstrumentation);
     }
 
-    //创建Activity实例的调用该方法
+    //第二步，startActivity的时候最终会执行Instrumentation的execStartActivity
+    public ActivityResult execStartActivity(Context who,
+                                            IBinder contextThread,
+                                            IBinder token,
+                                            Activity target,
+                                            Intent intent,
+                                            int requestCode,
+                                            Bundle options) {
+        injectIntent(intent);
+        return Reflect.on(base).call("execStartActivity", who, contextThread, token, target, intent, requestCode, options).get();
+    }
+
+    public void injectIntent(Intent intent) {
+        //换马甲，component是在manifest中注册的站位Activity，
+        ComponentName component = new ComponentName("com.example.pluginapp", "com.example.pluginapp.HookStubActivity");
+        //插件中的页面的跳转信息，真正要跳转的页面
+        ComponentName oldComponent = intent.getComponent();
+        if (component.getPackageName().equals(realContext.getPackageName())) {
+            //设置站位Activity的信息
+            intent.setComponent(component);
+            //但是却把真正要跳转的component设置参数，也就是用StubActivity传递插件的页面数据
+            intent.putExtra(KEY_COMPONENT, oldComponent);
+        }
+    }
+
+    //第三步，创建Activity实例的调用该方法
     @Override
     public Activity newActivity(@Nullable ClassLoader cl, @Nullable String className, @Nullable Intent intent) {
         assert intent != null;
         ComponentName componentName = intent.getParcelableExtra(KEY_COMPONENT);
         try {
             if (componentName != null) {
+                //获取要跳转的页面的全类名Class对象
                 Class<?> clazz = pluginContext.getClassLoader().loadClass(componentName.getClassName());
                 intent.setComponent(componentName);
+                //创建新的Activity实例
                 return (Activity) clazz.newInstance();
             }
         } catch (ClassNotFoundException e) {
@@ -91,27 +118,4 @@ public class AppInstrumentation extends Instrumentation {
         super.callActivityOnCreate(activity, icicle, persistentState);
     }
 
-    //startActivity的时候最终会执行Instrumentation的execStartActivity
-    public ActivityResult execStartActivity(Context who,
-                                            IBinder contextThread,
-                                            IBinder token,
-                                            Activity target,
-                                            Intent intent,
-                                            int requestCode,
-                                            Bundle options) {
-        injectIntent(intent);
-        return Reflect.on(base).call("execStartActivity", who, contextThread, token, target, intent, requestCode, options).get();
-    }
-
-    public void injectIntent(Intent intent) {
-        //换马甲，component是在manifest中注册的站位Activity，
-        ComponentName component = new ComponentName("com.example.pluginapp", "com.example.pluginapp.HookStubActivity");
-       //插件中的页面的跳转信息
-        ComponentName oldComponent = intent.getComponent();
-
-        if (component.getPackageName().equals(realContext.getPackageName())) {
-            intent.setComponent(component);
-            intent.putExtra(KEY_COMPONENT, oldComponent);
-        }
-    }
 }
